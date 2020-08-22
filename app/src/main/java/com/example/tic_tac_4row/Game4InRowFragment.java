ad8e13f0e40f61cea0462a473255aedd69c70855
farfotfir@gmail.com <fatemeasadi.1033@yahoo.com>
2020-08-22 21:33:41 +0430

    add two empty fragment for tic tac and 4 in row . add two button in main.xml for change fragment.

---
+++ app/src/main/java/com/example/tic_tac_4row/Game4InRowFragment.java
@@ -0,0 +1,31 @@
+package com.example.tic_tac_4row;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Game4InRowFragment extends Fragment {
+
+    public Game4InRowFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.fragment_game4_in_row, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/tic_tac_4row/GameTicFragment.java
@@ -0,0 +1,32 @@
+package com.example.tic_tac_4row;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameTicFragment extends Fragment {
+
+    public GameTicFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.fragment_game_tic, container, false);
+
+        return view;
+    }
+}
